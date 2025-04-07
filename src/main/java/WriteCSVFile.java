bc36b6016b6035728e66c1da599705a87eb023ad
Ujjwal <Ujjwalrawat248@gmail.com>
2025-04-07 14:16:26 +0530

    [Author:Ujjwal] ADD:WriteCSVFile.java

--- src/main/java/WriteCSVFile.java
+++ src/main/java/WriteCSVFile.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WriteCSVFile {
+    public static void main(String[] args) {
+        try(BufferedWriter writer=new BufferedWriter(new FileWriter("Employee.csv"))){
+            writer.write("ID,Name,Department,Salary\n");
+            writer.write("104,Alice Williams,Finance,62000\n");
+            writer.write("105,Bob Johnson,Sales,58000\n");
+            System.out.println("CSV file written successfully!");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
